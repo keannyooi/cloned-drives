16bbbb8f5d56e04d99151cb0ec0f9c6e5cba364e
Keanny Ooi <keannyooi@gmail.com>
2022-07-10 10:37:05 +0800

    updated ids

--- src/commands/benchmark.js
+++ src/commands/benchmark.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { registerFont, loadImage, createCanvas } = require("canvas");
+const { MessageAttachment } = require("discord.js");
 const { InfoMessage } = require("../util/classes/classes.js");
 
 module.exports = {
@@ -8,17 +10,18 @@ module.exports = {
     args: 0,
     category: "Testing",
     description: "A test command for benchmark tests. May also be repurposed for testing throwaway code.",
-    execute(message) {
+    async execute(message) {
         //setup
-        
+
         // test condition 1
         const test1Start = performance.now();
-
+        let attachment = await generate();
+        await message.channel.send({ files: [attachment] });
         const test1End = performance.now();
 
         // test condition 2
         const test2Start = performance.now();
-
+        await  message.channel.send({ files: ["https://cdn.discordapp.com/attachments/995376525119586356/995376817370308658/poc.png"] });
         const test2End = performance.now();
 
         const resultMessage = new InfoMessage({
@@ -30,8 +33,27 @@ module.exports = {
                 { name: "Test Condition 2", value: `\`${(test2End - test2Start).toFixed(3)}ms\``, inline: true }
             ]
         });
-        return resultMessage.sendMessage();
+        await resultMessage.sendMessage();
 
         //additional functions
+        async function generate() {
+            registerFont("RobotoCondensed-Regular.ttf", { family: "Roboto Condensed" });
+            const canvas = createCanvas(500, 304);
+            const ctx = canvas.getContext("2d");
+            const hud = await loadImage("https://cdn.discordapp.com/attachments/995376525119586356/995376817370308658/poc.png");
+            ctx.drawImage(hud, 0, 0, 500, 304);
+
+            ctx.textAlign = "right";
+            ctx.fillStyle = "#ffffff";
+            ctx.font = '55px "Roboto Condensed"';
+            ctx.fillText("130", 492, 50);
+            ctx.fillText("6.0", 492, 111);
+            ctx.fillText("83", 492, 173);
+            ctx.fillText("RWD", 492, 232);
+            ctx.fillText("SLK", 492, 292);
+
+            let attachment = new MessageAttachment(canvas.toBuffer(), "event.png");
+            return attachment;
+        }
     }
 };
