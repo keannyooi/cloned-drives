6231b872cab351d9b011744dd92e8cbacf660951
Keanny Ooi <keannyooi@gmail.com>
2022-08-27 08:18:04 +0800

    updated ids

--- src/util/functions/generateHud.js
+++ src/util/functions/generateHud.js
@@ -22,11 +22,14 @@ async function generateHud(currentCar, upgrade) {
     if (accel === 99.9) {
         accel = "N/A";
     }
+    else {
+        accel = accel.toFixed(1)
+    }
     ctx.textAlign = "right";
     ctx.fillStyle = "#ffffff";
     ctx.font = '55px "Roboto Condensed"';
     ctx.fillText(upgrade !== "000" ? bmReference[`${upgrade}TopSpeed`] : bmReference["topSpeed"], 492, 50);
-    ctx.fillText(accel.toFixed(1), 492, 111);
+    ctx.fillText(accel, 492, 111);
     ctx.fillText(upgrade !== "000" ? bmReference[`${upgrade}Handling`] : bmReference["handling"], 492, 173);
     ctx.fillText(bmReference["driveType"], 492, 237);
     ctx.fillText(tyreAbbrevs[bmReference["tyreType"]], 492, 292);
