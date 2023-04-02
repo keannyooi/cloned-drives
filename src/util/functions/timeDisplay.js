b70798bdc2c0077d6ae96b4689ece48eb4543fac
bnuuy <85796625+Bnuuy-chan@users.noreply.github.com>
2023-04-02 14:55:34 +0100

    try 3

--- src/util/functions/timeDisplay.js
+++ src/util/functions/timeDisplay.js
@@ -1,14 +1,16 @@
 "use strict";
 
 function timeDisplay(interval) {
-if (interval == null) {
-return "unlimited";
-}
-let days = Math.floor(interval.length("days"));
-let hours = Math.floor(interval.length("hours") - (days * 24));
-let minutes = Math.floor(interval.length("minutes") - (days * 1440) - (hours * 60));
-let seconds = Math.floor(interval.length("seconds") - (days * 86400) - (hours * 3600) - (minutes * 60));
-return `\`${days}d ${hours}h ${minutes}m ${seconds}s\``;
+    if (!interval) {
+        return "Unlimited";
+    }
+
+    let days = Math.floor(interval.length("days"));
+    let hours = Math.floor(interval.length("hours") - (days * 24));
+    let minutes = Math.floor(interval.length("minutes") - (days * 1440) - (hours * 60));
+    let seconds = Math.floor(interval.length("seconds") - (days * 86400) - (hours * 3600) - (minutes * 60));
+
+    return `\`${days}d ${hours}h ${minutes}m ${seconds}s\``;
 }
 
 module.exports = timeDisplay;
