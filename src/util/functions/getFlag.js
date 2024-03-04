2a22a9a16aeecf9063a4447c3360efb07bd8ee49
bnuuy <85796625+Bnuuy-chan@users.noreply.github.com>
2024-03-04 20:20:40 +0000

    fix flags + 118 cars

--- src/util/functions/getFlag.js
+++ src/util/functions/getFlag.js
@@ -4,12 +4,7 @@ function getFlag(code) {
     if (code.length !== 2) {
         return null;
     }
-    switch (code) {
-        case "YU":
-            return "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag_of_Yugoslavia_%281946-1992%29.svg/1000px-Flag_of_Yugoslavia_%281946-1992%29.svg.png";
-        default:
-            return `https://getflags.net/img1000/${code.toLowerCase()}.png`;
+            return `https://cd2.linkh.at/${code.toLowerCase()}`;
     }
-}
 
 module.exports = getFlag;
